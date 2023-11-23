370c902a819aa5bc025abb05e05efba40231d4ad
Srinidhi Kulkarni <93462562+srinidhifd@users.noreply.github.com>
2023-11-23 12:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function categorynews(button){
 
 
    
-if ((window.location.pathname === "https://news-hub-website.netlify.app/" || window.location.pathname === "https://news-hub-website.netlify.app/index.html" )) {
+if ((window.location.pathname === "/" || window.location.pathname === "/index.html" )) {
     // The current page is index.html
     let query = "india";
     fetchNews(query);
